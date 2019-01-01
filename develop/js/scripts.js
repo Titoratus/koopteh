8631b2cced35f0bc88c3bcad3683ca3f1206fa46
Titoratus <noxioustoo@yandex.ru>
2019-01-01 20:59:25 +0300

    Commit

---
+++ develop/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$('.wp-block-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+		}
+	});
+});
